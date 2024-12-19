fe02a2b60db6df58f5e75310093ae1cb29fe5f83
alxcreate <alx@alxcreate.com>
2024-12-19 23:26:52 +0500

    Fix styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -8,33 +7,33 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 
 library.add(fab, fas);
 
-import '../styles/styles-index.css';
+import styles from '../styles/styles-index.module.css';
 
 export default function Home(): JSX.Element {
   return (
     <Layout>
-      <main className="home-page">
-        <section className="profile-section">
-          <div className="profile-info">
+      <main className={styles['home-page']}>
+        <section className={styles['profile-section']}>
+          <div className={styles['profile-info']}>
             <p>Aleksey Abramov</p>
             <p>System Engineer</p>
           </div>
-          <div className="profile-vertical-line"></div>
-          <hr className="profile-horizontal-line" />
-          <div className="profile-contacts-container">
-            <div className="profile-contact-item">
+          <div className={styles['profile-vertical-line']}></div>
+          <hr className={styles['profile-horizontal-line']} />
+          <div className={styles['profile-contacts-container']}>
+            <div className={styles['profile-contact-item']}>
               <a href="mailto:alx@alxcreate.com">
                 <FontAwesomeIcon icon={['fas', 'envelope']} />
                 alx@alxcreate.com
               </a>
             </div>
-            <div className="profile-contact-item">
+            <div className={styles['profile-contact-item']}>
               <a href="https://www.linkedin.com/in/alxcreate" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={['fab', 'linkedin']} />
                 LinkedIn
               </a>
             </div>
-            <div className="profile-contact-item">
+            <div className={styles['profile-contact-item']}>
               <a href="https://t.me/alxcreate1" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={['fab', 'telegram']} />
                 Telegram
